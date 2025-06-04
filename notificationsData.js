aeed92066f5550ecdc1d3a2996d646b391ddf4cd
petr791 <fisherman003210gmail.com>
2025-06-04 21:51:51 +0300

    creat app

---
+++ notificationsData.js
@@ -0,0 +1,109 @@
+export const notificationsArray = {
+	"data": {
+		"notifications": [
+
+			{
+				"analyticParams": {
+					"campaign_id": "34288",
+					"group_id": "141654",
+					"launch_id": "1217570",
+					"notification_id": "nc.push.email.serv.c2c.trig.rec.crm4932.reco.for.se",
+					"notification_message_id": "682f592d13cbcdc3ad072ae922475302",
+					"ns_channel": "notificationCenter",
+					"project": "crm-async-launcher",
+					"target_hash": "22475302",
+					"target_type": 1,
+					"uid": 22475302
+				},
+				"date": 1747933486000,
+				"description": "Отобрали предложения, которые могут вас заинтересовать",
+				"id": "682f592e5f1cf96add2a6342",
+				"isRead": false,
+				"title": "Подборка исполнителей 🐗",
+				"uri": "recommendation"
+			},
+			{
+				"analyticParams": {
+					"campaign_id": "35631",
+					"group_id": "78685",
+					"launch_id": "1217031",
+					"notification_id": "push.nc.serv.c2c.trig.exp.crm5981.free.photo",
+					"notification_message_id": "682f3510247018f07f8aa0df22475302",
+					"ns_channel": "notificationCenter",
+					"project": "crm-async-launcher",
+					"target_hash": "22475302",
+					"target_type": 1,
+					"uid": 22475302
+				},
+				"date": 1747924245000,
+				"description": "Можно сняться для портфолио фотографа или получить услугу в обмен на отзыв",
+				"id": "682f3515f95789c9883c6711",
+				"isRead": false,
+				"title": "Фотоуслуги бесплатно 📷",
+				"uri": "general"
+			},
+			{
+				"analyticParams": {
+					"campaign_id": "36534",
+					"group_id": "88276",
+					"launch_id": "1216697",
+					"notification_id": "nc.push.serv.c2c.trig.rec.crm6695.podborka.top.holidays.v3",
+					"notification_message_id": "682e0c06eda7423e76053e3a22475302",
+					"ns_channel": "notificationCenter",
+					"project": "crm-async-launcher",
+					"target_hash": "22475302",
+					"target_type": 1,
+					"uid": 22475302
+				},
+				"date": 1747848200000,
+				"description": "Подобрали исполнителей в категории «Праздники, мероприятия» с рейтингом от 4,8",
+				"id": "682e0c07f3c3b27703eb9415",
+				"isRead": false,
+				"title": "Высокая оценка ⭐️",
+				"uri": "general"
+			},
+			{
+				"analyticParams": {
+					"campaign_id": "43747",
+					"group_id": "162978",
+					"launch_id": "1216761",
+					"notification_id": "mess.nc.gen.c2c.one.cross.crm12408.buyers.apps",
+					"notification_message_id": "682df146f69117cd473e5c3c22475302",
+					"ns_channel": "notificationCenter",
+					"project": "crm-async-launcher",
+					"target_hash": "22475302",
+					"target_type": 1,
+					"uid": 22475302
+				},
+				"date": 1747841350000,
+				"description": "Если вещь больше не приносит пользу, её можно предложить другим",
+				"id": "682df146f3c3b27703dc595b",
+				"isRead": false,
+				"title": "Заработайте на Авито 🫰",
+				"uri": "recommendation"
+			},
+			{
+				"analyticParams": {
+					"campaign_id": "32621",
+					"group_id": "48864",
+					"launch_id": "1215488",
+					"notification_id": "push.nc.job.c2c.trig.exp.crm3664.look.vacancies.var2",
+					"notification_message_id": "682c3c2e0e19bdda21ff04a522475302",
+					"ns_channel": "notificationCenter",
+					"project": "crm-async-launcher",
+					"target_hash": "22475302",
+					"target_type": 1,
+					"uid": 22475302
+				},
+				"date": 1747729460000,
+				"description": "Эти вакансии могут вас заинтересовать — взгляните",
+				"id": "682c3c346a8c924dcd91f536",
+				"isRead": false,
+				"title": "Работа в Москве 🏢",
+				"uri": "general"
+			},
+
+		]
+	},
+	"status": "success"
+}
